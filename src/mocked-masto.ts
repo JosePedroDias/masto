66d51516c79fad90321f167a9bef786b78a90354
Jose Pedro Dias <jose.pedro.dias@gmail.com>
2022-11-16 17:19:30 +0000

    added more meaningful post examples in to easily tweak the generated HTML/CSS

--- src/mocked-masto.ts
+++ src/mocked-masto.ts
@@ -4,7 +4,7 @@ import { Persistence } from './persistence';
 
 export async function getHomeTimeline(per: Persistence): Promise<Array<Entity.Status>> {
     const simple = (await import('./test-toots/simple.json', { assert: { type: "json" } })).default;
-    //const reblog = (await import('./test-toots/reblog.json', { assert: { type: "json" } })).default;
+    const reblog = (await import('./test-toots/reblog.json', { assert: { type: "json" } })).default;
     const cw = (await import('./test-toots/cw.json', { assert: { type: "json" } })).default;
     const poll = (await import('./test-toots/poll.json', { assert: { type: "json" } })).default;
     //const langFr = (await import('./test-toots/lang-fr.json', { assert: { type: "json" } })).default;
@@ -12,12 +12,13 @@ export async function getHomeTimeline(per: Persistence): Promise<Array<Entity.St
     const urlLetterboxd = (await import('./test-toots/url-letterboxd.json', { assert: { type: "json" } })).default;
     const urlObject = (await import('./test-toots/url-object.json', { assert: { type: "json" } })).default;
     const urlSpotify = (await import('./test-toots/url-spotify.json', { assert: { type: "json" } })).default;
-    //const urlTwitter = (await import('./test-toots/url-twitter.json', { assert: { type: "json" } })).default;
+    const urlTwitter = (await import('./test-toots/url-twitter.json', { assert: { type: "json" } })).default;
     const urlYoutube = (await import('./test-toots/url-youtube.json', { assert: { type: "json" } })).default;
 
 
     return Promise.resolve([
         simple,
+        reblog,
         cw,
         poll,
         //langFr,
@@ -25,6 +26,7 @@ export async function getHomeTimeline(per: Persistence): Promise<Array<Entity.St
         urlLetterboxd,
         urlObject,
         urlSpotify,
+        urlTwitter,
         urlYoutube
     ] as any as Array<Entity.Status>);
 }
